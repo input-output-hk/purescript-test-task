d3f63926857359c473ce2e9bf7f9c03e4ba605b4
Jonn Mostovoy <amarr.industrial@gmail.com>
2016-11-14 06:39:33 +0200

    Add trackingExample, implement Track typeclass

--- src/Sample.hs
+++ src/Sample.hs
@@ -1,13 +1,50 @@
+{-# LANGUAGE FlexibleInstances     #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedStrings     #-}
 
-module Sample where
+module Sample
+  (Track(extract)
+  ,Report(produce)
+  ,Input()
+  ,Output()
+  ,rawData
+  ,trackingExample)
+  where
 
-class Track e  where
+import           Data.Text             (Text ())
+import           Data.Time.Clock       (NominalDiffTime ())
+import           Data.Time.Clock.POSIX (POSIXTime ())
+
+class Track e t b a where
     extract
         :: (Foldable b, Monoid (t (b a)), Traversable t)
-        => e -> t (b a)
+        => e -> t (b a) -> t (b a)
 
 class Report d r  where
     produce
         :: (Monoid (t d), Traversable t)
         => (t d -> r)
+
+type Input = (Text, POSIXTime)
+
+type Output = (Text, NominalDiffTime)
+
+instance Track [Input] [] [] Output where
+    extract xs ds = ds `mappend` fst (foldl f ([], undefined) xs)
+      where
+        f ([ys], (yu, v0)) (u,v) = ([((yu, v - v0) : ys)], (u, v))
+        f _                (u,v) = ([[]],                  (u, v))
+
+speakers :: [Text]
+speakers = ["Jonn", "Charles", "Arseniy", "Jonn", "George", "Meeting Ended"]
+
+timeStamps :: [Integer]
+timeStamps =
+    [1479081309, 1479081429, 1479081666, 1479082000, 1479083307, 1479085307]
+
+rawData :: [Input]
+rawData = zipWith (\x y -> (x, fromIntegral y)) speakers timeStamps
+
+trackingExample :: [[Output]]
+trackingExample = extract rawData []
+
